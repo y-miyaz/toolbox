36ade88fa8fd7af881c61456a16a02bfe9f26592
yohei.miyazaki <jyakly3@gmail.com>
2018-11-25 04:00:54 +0900

    first commit

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  assetsDir: '../../dumy_burier/static',
+  baseUrl: undefined,
+  outputDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
